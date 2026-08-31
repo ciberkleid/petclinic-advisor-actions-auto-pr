2da1c4b7d64a53f30f1eb92917e3817597f43105
Cora Iberkleid <ciberkleid@gmail.com>
2026-08-30 20:30:04 -0400

    [Auto] Upgrades Spring Boot from 4.0.x to 4.1.x

--- src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
@@ -50,7 +50,7 @@ class PetClinicIntegrationTests {
 
 	@Test
 	void testOwnerDetails() {
-		RestTemplate template = builder.rootUri("http://localhost:" + port).build();
+		RestTemplate template = builder.baseUri("http://localhost:" + port).build();
 		ResponseEntity<String> result = template.exchange(RequestEntity.get("/owners/1").build(), String.class);
 		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
